2db85378b4276d2e570b3806a1fe2ed324fdd98e
Daniel Smith <Daniel.Smith.ITguy@gmail.com>
2016-02-22 11:19:59 -0700

    updating with homework LRTHW chapter 2 after rebase
    [#11391075]

---
+++ chapter_2/ex2.rb
@@ -0,0 +1,9 @@
+# a comment, this is so you can read your program later.
+# anything after the # is ingored by ruby.
+
+puts 'i could have like this.' # and the comment after is ingored.
+
+# You could also use comment to "disable" or comment out pice of code:
+# puts "This won't run."
+
+puts 'this will run.'
