f0221a49ec4b110f89348b65f299900dbc317d2f
Daniel Smith <Daniel.Smith.ITguy@gmail.com>
2016-03-09 15:27:08 -0700

    made PR changes
    [#113791083]

--- chapter_6/Readme.md
+++ chapter_6/Readme.md
@@ -1,8 +1,12 @@
 >###Find all the places where a string is put inside a string. There are four places.
 
-```x = "There are #{types_of_people} types of people."```
-```y = "Those who know #{binary} and those who #{do_not}."```
-```joke_evaluation = "Isn't that joke so funny?! #{hilarious}"```
+```
+x = "There are #{types_of_people} types of people."
+
+y = "Those who know #{binary} and those who #{do_not}."
+
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+```
 
 
 >###Are you sure there are only four places? How do you know? Maybe I like lying.
@@ -15,4 +19,4 @@ It's like adding, only concatenation, takes one and 'adds' it to the end.
 
 What happens when you change the strings to use ' (single-quote) instead of " (double-quote)? Do they still work? Try to guess why.
 
-nothing if you have something like ```'test'``` if you have ```'#{test}'``` then it will not work, "" tells ruby to evaluate with in them.
+nothing if you have something like `'test'` if you have `'#{test}'` then it will not work, "" tells ruby to evaluate with in them.
--- chapter_6/ex6.rb
+++ chapter_6/ex6.rb
@@ -1,22 +1,32 @@
-#not sure what the commit here, all of it is self explanatory
+# not sure what the commit here, all of it is self explanatory
+# types_of_people is assigned 10
 types_of_people = 10
+# x is assigned "There are 10 types of people."
 x = "There are #{types_of_people} types of people."
+# binary is assigned "binary"
 binary = "binary"
+# do_not is assigned "don't"
 do_not = "don't"
+# y is assigned "Those who know binary and those who don't."
 y = "Those who know #{binary} and those who #{do_not}."
-
+# print x
 puts x
+# print y
 puts y
-
+# print x with I said: before
 puts "I said: #{x}."
+# prints y with I also said: before
 puts "I also said: '#{y}'."
-
+# hilarious is assigned false
 hilarious = false
+# joke_evaluation is assigned "Isn't that joke so funny?! #{hilarious}"
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
-
+# print joke_evaluation
 puts joke_evaluation
-
+# w is assigned "This is the left side of..."
 w = "This is the left side of..."
+# e is assigned "a string with a right side."
 e = "a string with a right side."
-
+#print w and e
 puts w + e
+#still all self explanatory, why am I doing this if I already have this fundamental understanding?
